cdb8610e93872f283cc125e7cf0824e02e9f6e9b
JP Bois <jp.bois.06@outlook.fr>
2022-08-02 18:40:09 +0200

    Bumping dependencies (#6)
    * chore: bumping dependencies
    
    * chore: serving swagger at /api path
    
    * chore: amending readme

--- libs/boostraper/src/index.ts
+++ libs/boostraper/src/index.ts
@@ -23,7 +23,7 @@ export const bootstrap = async (
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('', app, document);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(port, '0.0.0.0');
 
